aec91cbaaf902a386e1a902c3860f7c9477bcb57
pokmnx <budpok@outlook.com>
2018-07-10 17:55:35 +0800

    Fix styling

--- Android/app/src/main/java/com/storageauctions/dev/WelcomeFragment.java
+++ Android/app/src/main/java/com/storageauctions/dev/WelcomeFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class WelcomeFragment extends Fragment {
 
@@ -14,6 +16,27 @@ public class WelcomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        Button claimFacility = (Button) view.findViewById(R.id.claim_facility_btn);
+        Button addFirstAuction = (Button) view.findViewById(R.id.add_first_auction_btn);
+
+        claimFacility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new FacilitySearchFragment();
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, fragment);
+                ft.commit();
+            }
+        });
+
+        addFirstAuction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return view;
     }
 }
